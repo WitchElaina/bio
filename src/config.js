b1364e0df89c7ab0e8bb5edaa075d4d230e47df3
WitchElaina <msz13399173387@gmail.com>
2023-06-02 01:43:30 +0800

    fix path

--- src/config.js
+++ src/config.js
@@ -5,7 +5,7 @@ export default {
   BIO: 'I am a Frontend Developer and IoT enthusiast. I love to learn new things and share my knowledge with others.',
   SKILLS:
     'HTML, CSS, JavaScript, Vue.js, React.js, Node.js, Express.js, MongoDB, MySQL, Python, C, C++, Java, Git, Arduino, Raspberry Pi, ESP32, ESP8266, MQTT, REST API, Firebase, Heroku, Netlify, Vercel, DigitalOcean, Linux, Windows, Mac OS',
-  AVATAR: '/ava.png',
+  AVATAR: 'ava.png',
   SOCIAL_LINKS: [
     {
       label: 'fa-brands fa-twitter',
