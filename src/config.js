838d858cedde18a6bcba806a8157908e2df7a8cb
WitchElaina <msz13399173387@gmail.com>
2023-06-02 01:39:48 +0800

    Add codes

---
+++ src/config.js
@@ -0,0 +1,19 @@
+export default {
+  NAME: 'WitchElaina',
+  BASE_URL: 'https://witchelaina.github.io/',
+  TAGS: ['Frontend Developer', 'IoT'],
+  BIO: 'I am a Frontend Developer and IoT enthusiast. I love to learn new things and share my knowledge with others.',
+  SKILLS:
+    'HTML, CSS, JavaScript, Vue.js, React.js, Node.js, Express.js, MongoDB, MySQL, Python, C, C++, Java, Git, Arduino, Raspberry Pi, ESP32, ESP8266, MQTT, REST API, Firebase, Heroku, Netlify, Vercel, DigitalOcean, Linux, Windows, Mac OS',
+  AVATAR: '/ava.png',
+  SOCIAL_LINKS: [
+    {
+      label: 'fa-brands fa-twitter',
+      url: 'https://twitter.com/Mszook2002',
+    },
+    {
+      label: 'fa-brands fa-github',
+      url: 'https://github.com/witchelaina',
+    },
+  ],
+};
---
+++ src/views/HomeView.vue
@@ -0,0 +1,37 @@
+<script setup>
+import IconButton from '../components/IconButton.vue';
+import config from '../config';
+</script>
+
+<template>
+  <div class="home-wrapper">
+    <IconButton label="fas fa-chevron-up" color="var(--color-on-main-card-secondary)"></IconButton>
+    <div class="ui-tips">Scroll to see more</div>
+    <div class="bio">{{ config.BIO }}</div>
+  </div>
+</template>
+
+<style scoped>
+.home-wrapper {
+  position: absolute;
+  top: 0;
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  padding-top: 20px;
+}
+
+.ui-tips {
+  color: var(--color-on-main-card-secondary);
+  font-size: 20px;
+}
+
+.bio {
+  color: var(--color-on-main-card-primary);
+  font-size: 25px;
+  margin-top: 20px;
+  text-align: center;
+}
+</style>
